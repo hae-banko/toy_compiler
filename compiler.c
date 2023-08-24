b9cd6cc30a5c34bcba7ae8f347facd24aa28102d
Irvan F <fikardi@rfmondial.de>
2023-08-24 19:39:30 +0200

    basic compiler process added.

--- compiler.c
+++ compiler.c
@@ -1,4 +1,21 @@
+#include "compiler.h"
+
+/* Compile file will pass any .c file into the function and creating the object file.
+ * This will return a 0.
+ */
+
 int compile_file(const char* filename, const char * out_filename, int flags)
 {
-    return 0;
+    struct compile_process* process = compile_process_create(filename, out_filename, flags);
+    if(!process) return COMMPILER_FAILED_WITH_ERRORS;
+
+    // perform lexical analysis
+
+    // perform parsing
+
+    // perform code generation
+
+    // usw...
+
+    return COMPILER_FILE_COMPILED_OK;
 }
